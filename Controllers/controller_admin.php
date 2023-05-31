348961fd646d6db6715204dd52f6f72a1a81c0da
pramadani <araryap@gmail.com>
2023-05-31 12:50:56 +0700

    HAO

---
+++ Controllers/controller_admin.php
@@ -0,0 +1,68 @@
+<?php
+
+include_once("../Models/model_admin.php");
+
+class controller_admin
+{
+    public $model;
+
+    public function __construct()
+    {
+        $this->model = new model_admin();
+    }
+    public function vid()
+    {
+        $data = $this->model->getSemuaData();
+        include 'v_data.php';
+    }
+
+    public function add($nim, $nama, $prodi, $email)
+    {
+        session_start();
+
+        $e = $this->model->setData($nim, $nama, $prodi, $email);
+
+        if ($e == "") {
+            header("location:../data/");
+        } else {
+            $_SESSION['temp'] = $e;
+            header("location:../tambah/");
+        }
+    }
+
+    public function delete($nim)
+    {
+        $this->model->deleteRow($nim);
+
+        header("location:../data/");
+    }
+
+    public function update($no, $nim, $nama, $prodi, $email)
+    {
+        $e = $this->model->updateRow($no, $nim, $nama, $prodi, $email);
+        header("location:../data/");
+    }
+
+    public function row($no)
+    {
+        return $data = $this->model->getRow($no);
+    }
+
+    public function log()
+    {
+        session_start();
+        $user = $_POST["user"];
+        $pass = $_POST["pass"];
+        $data = $this->model->login($user, $pass);
+
+        if ($data == "admin") {
+            $_SESSION['username'] = 'admin';
+            header("location:../data/");
+        } else {
+            $_SESSION['temp'] = "error";
+            header("location:../login/");
+        }
+
+        
+    }
+}
--- Models/model_admin.php
+++ Models/model_admin.php
@@ -2,32 +2,84 @@
 require "./Models/opt/connection.php";
 class model_admin
 {
-    private $email;
-    private $no_telp;
+    private $id;
     private $nama;
-    private $tanggal_lahir;
-    private $jenis_kelamin;
-    private $divisi;
-    private $password;
-    
-    public function setRowAdmin($email, $no_telp, $nama, $tanggal_lahir, $jenis_kelamin, $divisi, $password)
-    {
-        $this->email = $email;
-        $this->no_telp = $no_telp;
-        $this->nama = $nama;
-        $this->tanggal_lahir = $tanggal_lahir;
-        $this->jenis_kelamin = $jenis_kelamin;
-        $this->divisi = $divisi;
-        $this->password = $password;
+    private $kelas;
+    private $fakta_unik;
+    private $keterangan;
+    private $habitat;
+    private $makanan;
+    private $cara_hidup;
+    private $reproduksi;
+    private $ancaman;
+    private $foto_profil;
+    private $foto_halaman;
 
+    public function setData($id, $nama, $kelas, $fakta_unik, $keterangan, $habitat, $makanan, $cara_hidup, $reproduksi, $ancaman, $foto_profil, $foto_halaman)
+    {
+        $this->id = $id;
+        $this->nama = $nama;
+        $this->kelas = $kelas;
+        $this->fakta_unik = $fakta_unik;
+        $this->keterangan = $keterangan;
+        $this->habitat = $habitat;
+        $this->makanan = $makanan;
+        $this->cara_hidup = $cara_hidup;
+        $this->reproduksi = $reproduksi;
+        $this->ancaman = $ancaman;
+        $this->foto_profil = $foto_profil;
+        $this->foto_halaman = $foto_halaman;
         global $mysqli;
-        
+
         try {
-            $mysqli->query("INSERT INTO admin_zoo VALUES ('$this->email', '$this->no_telp', '$this->nama', '$this->tanggal_lahir', '$this->jenis_kelamin, '$this->divisi', '$this->password')");
+            $mysqli->query("INSERT INTO list_binatang VALUES ('$this->id', '$this->nama', '$this->kelas', '$this->fakta_unik', '$this->keterangan', '$this->habitat', '$this->makanan', '$this->cara_hidup', '$this->reproduksi', '$this->ancaman', ' $this->foto_profil', '$this->foto_halaman')");
             return $e = "";
         } catch (mysqli_sql_exception $e) {
             return $e;
         }
+    }
+
+    public function getSemuaData()
+    {
+        global $mysqli;
+        $rs = $mysqli->query("SELECT * FROM list_binatang");
+        $rows = array();
+        while ($row = $rs->fetch_assoc()) {
+            $rows[] = $row;
+        }
+
+        return $rows;
+    }
+
+    public function deleteRow($id)
+    {
+        global $mysqli;
+
+        $mysqli->query("DELETE FROM list_binatang WHERE id = '$id'");
+    }
+
+    public function updateRow($no, $id, $nama, $kelas, $fakta_unik, $keterangan, $habitat, $makanan, $cara_hidup, $reproduksi, $ancaman, $foto_profil, $foto_halaman)
+    {
+        global $mysqli;
+
+        try {
+            $mysqli->query("UPDATE list_binatang SET id='$id', nama='$nama', kelas='$kelas', fakta_unik='$fakta_unik', keterangan='$keterangan', habitat='$habitat', makanan='$makanan', cara_hidup='$cara_hidup', reproduksi='$reproduksi', ancaman='$ancaman', foto_profil='$foto_profil', foto_halaman='$foto_halaman' WHERE id='$no'");
+            return $e = "";
+        } catch (mysqli_sql_exception $e) {
+            return $e;
+        }
+    }
+
+    public function getRow($id)
+    {
+        global $mysqli;
+
+        $rs = $mysqli->query("SELECT * FROM list_binatang WHERE id = '$id'");
+        $rows = array();
+        while ($row = $rs->fetch_assoc()) {
+            $rows = $row;
+        }
+        return $rows;
     }
     
     public function getRowAdmin($email)
@@ -53,24 +105,4 @@ class model_admin
 
         return $rows;
     }
-
-    public function deleteRowAdmin($email)
-    {
-        global $mysqli;
-
-        $mysqli->query("DELETE FROM admin_zoo WHERE email = '$email'");
-    }
-
-    public function updateRowAdmin($id, $email, $no_telp, $nama, $tanggal_lahir)
-    {
-        global $mysqli;
-
-        try {
-            $mysqli->query("UPDATE admin_zoo SET email='$email', no_telp='$no_telp', nama='$nama', tanggal_lahir='$tanggal_lahir' WHERE email='$id'");
-            return $e = "";
-        } catch (mysqli_sql_exception $e) {
-            return $e;
-        }
-    }
-
 }
