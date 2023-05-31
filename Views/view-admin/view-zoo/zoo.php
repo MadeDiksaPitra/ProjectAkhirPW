365ad82cfce4002b2e6088baa10681d58ab56048
pramadani <araryap@gmail.com>
2023-05-31 15:39:31 +0700

    sz

--- Views/view-admin/view-zoo/zoo.php
+++ Views/view-admin/view-zoo/zoo.php
@@ -1,37 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Admin</title>
 </head>
+
 <body>
     <table class="tabel">
         <tr>
-            <th>NIM</th>
+            <th>id</th>
             <th>Nama</th>
-            <th>Program Studi</th>
-            <th>Email</th>
-            <th>Ubah</th>
-            <th>Hapus</th>
+            <th>kelas</th>
+            <th>fu</th>
+            <th>ket</th>
+            <th>Hapushabit</th>
+            <th>makanan</th>
+            <th>cara hidup</th>
+            <th>reproduksi</th>
+            <th>ancaman</th>
+            <th>fp</th>
+            <th>fh</th>
         </tr>
+
         <?php
-        include "koneksi.php";
-        $result = $mysqli->query("SELECT * FROM datamahasiswa");
-        while ($view_tabel = mysqli_fetch_array($result)) {
-            echo "<div style=\"text-align:center\">";
-            echo "<tr>
-            <td>$view_tabel[nim]</td>
-            <td>$view_tabel[nama]</td>
-            <td>$view_tabel[programstudi]</td>
-            <td>$view_tabel[email]</td>";
-            echo
-            "<td><form method='POST' action='action.php?nim=$view_tabel[nim]'><input id='submit' type='submit' value='Edit'/></form></td>
-            <td><form method='POST' action='action1.php?nim=$view_tabel[nim]'><input id='submit' name='delete' onclick=\"show_alert()\" type='submit' value='Delete'/></form></td>
+        echo "<tr>
+            <td>$data[nim]</td>
+            <td>$data[nama]</td>
+            <td>$data[programstudi]</td>
+            <td>$data[email]</td>
             </tr>";
-        }
         ?>
     </table>
 </body>
+
 </html>
