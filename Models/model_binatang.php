b66f5d63c8b01983f2aa3760003dc18f226d971b
pramadani <araryap@gmail.com>
2023-05-31 13:13:23 +0700

    list

--- Models/model_binatang.php
+++ Models/model_binatang.php
@@ -2,84 +2,21 @@
 require "../opt/connection.php";
 class model_binatang
 {
-    private $id;
-    private $nama;
-    private $kelas;
-    private $fakta_unik;
-    private $keterangan;
-    private $habitat;
-    private $makanan;
-    private $cara_hidup;
-    private $reproduksi;
-    private $ancaman;
-    private $foto_profil;
-    private $foto_halaman;
-
-    public function setData($id, $nama, $kelas, $fakta_unik, $keterangan, $habitat, $makanan, $cara_hidup, $reproduksi, $ancaman, $foto_profil, $foto_halaman)
-    {
-        $this->id = $id;
-        $this->nama = $nama;
-        $this->kelas = $kelas;
-        $this->fakta_unik = $fakta_unik;
-        $this->keterangan = $keterangan;
-        $this->habitat = $habitat;
-        $this->makanan = $makanan;
-        $this->cara_hidup = $cara_hidup;
-        $this->reproduksi = $reproduksi;
-        $this->ancaman = $ancaman;
-        $this->foto_profil = $foto_profil;
-        $this->foto_halaman = $foto_halaman;
-        global $mysqli;
-
-        try {
-            $mysqli->query("INSERT INTO list_binatang VALUES ('$this->id', '$this->nama', '$this->kelas', '$this->fakta_unik', '$this->keterangan', '$this->habitat', '$this->makanan', '$this->cara_hidup', '$this->reproduksi', '$this->ancaman', ' $this->foto_profil', '$this->foto_halaman')");
-            return $e = "";
-        } catch (mysqli_sql_exception $e) {
-            return $e;
-        }
-    }
-
-    public function getSemuaData()
+    public function getListNama()
     {
         global $mysqli;
 
         $rs = $mysqli->query("SELECT nama FROM list_binatang");
-        // $rows = array();
-        // while ($row = $rs->fetch_assoc()) {
-        //     $rows[] = $row;
-        // }
 
         return $rs;
     }
 
-    // public function deleteRow($id)
-    // {
-    //     global $mysqli;
-
-    //     $mysqli->query("DELETE FROM list_binatang WHERE id = '$id'");
-    // }
-
-    public function updateRow($no, $id, $nama, $kelas, $fakta_unik, $keterangan, $habitat, $makanan, $cara_hidup, $reproduksi, $ancaman, $foto_profil, $foto_halaman)
+    public function getDeskripsi($id)
     {
         global $mysqli;
 
-        try {
-            $mysqli->query("UPDATE list_binatang SET id='$id', nama='$nama', kelas='$kelas', fakta_unik='$fakta_unik', keterangan='$keterangan', habitat='$habitat', makanan='$makanan', cara_hidup='$cara_hidup', reproduksi='$reproduksi', ancaman='$ancaman', foto_profil='$foto_profil', foto_halaman='$foto_halaman' WHERE id='$no'");
-            return $e = "";
-        } catch (mysqli_sql_exception $e) {
-            return $e;
-        }
+        $rs = $mysqli->query("SELECT * FROM list_binatang WHERE id = '$id'");
+
+        return $rs;
     }
-
-    // public function getRow($id)
-    // {
-    //     global $mysqli;
-
-    //     $rs = $mysqli->query("SELECT * FROM list_binatang WHERE id = '$id'");
-    //     $rows = array();
-    //     while ($row = $rs->fetch_assoc()) {
-    //         $rows = $row;
-    //     }
-    //     return $rows;
-    // }
 }
