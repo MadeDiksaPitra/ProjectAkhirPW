e3898b6e39e469bd10e38009dbd6a6d5bf2372e8
Made Diksa Pitra <madediksapitra@gmail.com>
2023-05-31 16:25:47 +0700

    Nambah Create

---
+++ Views/view-admin/view-create/create.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="./admin-create" method="post" class="">
+
+                <span class="indextambah-text4">ID</span>
+                <input type="text" name="id" placeholder="Masukkan ID" class="indextambah-textinput input" />
+
+                <span class="indextambah-text5">Nama</span>
+                <input type="text" name="nama" placeholder="Masukkan Nama" class="indextambah-textinput1 input" />
+
+                <span class="indextambah-text5">Kelas</span>
+                <input type="text" name="kelas" placeholder="Masukkan Kelas" class="indextambah-textinput1 input" />
+
+                <span class="indextambah-text5">Fakta Unik</span>
+                <input type="text" name="fakta_unik" placeholder="Masukkan Fakta Unik" class="indextambah-textinput1 input" />
+                
+                <span class="indextambah-text5">Keterangan</span>
+                <input type="text" name="keterangan" placeholder="Masukkan Keterangan" class="indextambah-textinput1 input" />
+
+                <span class="indextambah-text5">Habitat</span>
+                <input type="text" name="habitat" placeholder="Masukkan Habitat" class="indextambah-textinput1 input" />
+
+                <span class="indextambah-text5">Makanan</span>
+                <input type="text" name="makanan" placeholder="Masukkan Makanan" class="indextambah-textinput1 input" />
+
+                <span class="indextambah-text5">Cara Hidup</span>
+                <input type="text" name="cara_hidup" placeholder="Masukkan Cara Hidup" class="indextambah-textinput1 input" />
+
+                <span class="indextambah-text5">Reproduksi</span>
+                <input type="text" name="reproduksi" placeholder="Masukkan Reproduksi" class="indextambah-textinput1 input" />
+
+                <span class="indextambah-text5">Ancaman</span>
+                <input type="text" name="ancaman" placeholder="Masukkan Ancaman" class="indextambah-textinput1 input" />
+
+                <span class="indextambah-text5">Foto Profil</span>
+                <input type="text" name="foto_profil" placeholder="Masukkan Foto Profil" class="indextambah-textinput1 input" />
+
+                <span class="indextambah-text5">Foto Halaman</span>
+                <input type="text" name="foto_halaman" placeholder="Masukkan Foto Halaman" class="indextambah-textinput1 input" />
+
+            <button name="submit" type="submit" class="indextambah-button button">
+                Tambahkan
+            </button>
+        </div>
+    </form>
+</body>
+
+</html>
