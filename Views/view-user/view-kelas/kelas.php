c76d3b88d4a34fffecb2cdc38b446f1a71ee0842
Made Diksa Pitra <madediksapitra@gmail.com>
2023-06-03 19:43:56 +0700

    MANTAOPAP

--- Views/view-user/view-kelas/kelas.php
+++ Views/view-user/view-kelas/kelas.php
@@ -2,62 +2,268 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jenis Hewan</title>
+  <title>kelas - Arctic Avaricious Flamingo</title>
+  <meta property="og:title" content="kelas - Arctic Avaricious Flamingo" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8" />
+  <meta property="twitter:card" content="summary_large_image" />
+
+  <style data-tag="reset-style-sheet">
+    html {
+      line-height: 1.15;
+    }
+
+    body {
+      margin: 0;
+    }
+
+    * {
+      box-sizing: border-box;
+      border-width: 0;
+      border-style: solid;
+    }
+
+    p,
+    li,
+    ul,
+    pre,
+    div,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    figure,
+    blockquote,
+    figcaption {
+      margin: 0;
+      padding: 0;
+    }
+
+    button {
+      background-color: transparent;
+    }
+
+    button,
+    input,
+    optgroup,
+    select,
+    textarea {
+      font-family: inherit;
+      font-size: 100%;
+      line-height: 1.15;
+      margin: 0;
+    }
+
+    button,
+    select {
+      text-transform: none;
+    }
+
+    button,
+    [type="button"],
+    [type="reset"],
+    [type="submit"] {
+      -webkit-appearance: button;
+    }
+
+    button::-moz-focus-inner,
+    [type="button"]::-moz-focus-inner,
+    [type="reset"]::-moz-focus-inner,
+    [type="submit"]::-moz-focus-inner {
+      border-style: none;
+      padding: 0;
+    }
+
+    button:-moz-focus,
+    [type="button"]:-moz-focus,
+    [type="reset"]:-moz-focus,
+    [type="submit"]:-moz-focus {
+      outline: 1px dotted ButtonText;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: inherit;
+    }
+
+    input {
+      padding: 2px 4px;
+    }
+
+    img {
+      display: block;
+    }
+
+    html {
+      scroll-behavior: smooth
+    }
+  </style>
+  <style data-tag="default-style-sheet">
+    html {
+      font-family: Inter;
+      font-size: 16px;
+    }
+
+    body {
+      font-weight: 400;
+      font-style: normal;
+      text-decoration: none;
+      text-transform: none;
+      letter-spacing: normal;
+      line-height: 1.15;
+      color: var(--dl-color-gray-black);
+      background-color: var(--dl-color-gray-white);
+
+    }
+  </style>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" data-tag="font" />
+  <link rel="stylesheet" href="./Views/view-user/view-kelas/style.css" />
 </head>
 
 <body>
-    <h2>Jenis Hewan</h2>
-    <a href="./list-binatang?kelas=Mamalia" class="kolom-klik">
-        <?php
-        // Variabel dengan teks yang akan ditampilkan
-        $teks = "Mamalia merupakan bla bla bla";
-        ?>
+  <div>
+    <link href="./Views/view-user/view-kelas/kelas.css" rel="stylesheet" />
 
+    <div class="kelas-container">
+      <div class="kelas-container01">
+        <div class="kelas-container02">
+          <div class="kelas-container03">
+            <div class="kelas-container04">
+              <button type="button" class="kelas-button button">
+                Login Admin
+              </button>
+            </div>
+          </div>
+          <div class="kelas-container05"></div>
+        </div>
+        <div class="kelas-container06">
+          <div class="kelas-container07">
+            <span class="kelas-text">
+              <span class="kelas-text01">Jenis</span>
+              <span></span>
+              <br class="kelas-text03" />
+              <span class="kelas-text04">Hewan</span>
+            </span>
+            <div class="kelas-container08"></div>
+            <div class="kelas-container09"></div>
+          </div>
+          <div class="kelas-container10">
+            <div id="myButton" class="kelas-mamalia">
 
-        <h1>Mamalia</h1>
-        <p>
-            <?php echo $teks; ?>
-        </p>
-    </a>
+              <script>
+                document.getElementById("myButton").addEventListener("click", function() {
+                  window.location.href = "./list-binatang?kelas=Mamalia"
+                });
+              </script>
 
-    <a href="./list-binatang?kelas=Aves" class="kolom-klik">
-        <?php
-        // Variabel dengan teks yang akan ditampilkan
-        $teks = "Aves merupakan bla sajsadsd";
-        ?>
+              <div class="kelas-container11">
+                <div class="kelas-container12"></div>
+              </div>
+              <div class="kelas-container13">
+                <div class="kelas-container14">
+                  <span class="kelas-text05">Mamalia</span>
+                </div>
+                <div class="kelas-container15">
+                  <span class="kelas-text06">
+                    Mamalia adalah kelompok hewan yang memiliki ciri khas
+                    seperti memiliki kelenjar susu untuk menyusui anaknya,
+                    memiliki rambut atau bulu yang menutupi tubuh, dan
+                    memiliki gigi yang berbeda-beda untuk fungsi makanan.
+                    Mamalia hidup di berbagai habitat di seluruh dunia, mulai
+                    dari darat, laut, hingga udara.
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div id="myButton1" class="kelas-aves">
 
-        <h1>Aves</h1>
-        <p>
-            <?php echo $teks; ?>
-        </p>
-    </a>
+            <script>
+                document.getElementById("myButton1").addEventListener("click", function() {
+                  window.location.href = "./list-binatang?kelas=Aves"
+                });
+              </script>
 
-    <a href="./list-binatang?kelas=Reptilia" class="kolom-klik">
-        <?php
-        // Variabel dengan teks yang akan ditampilkan
-        $teks = "Reptilia merupakan hewan yang";
-        ?>
+              <div class="kelas-container16">
+                <div class="kelas-container17"></div>
+              </div>
+              <div class="kelas-container18">
+                <div class="kelas-container19">
+                  <span class="kelas-text07">Aves</span>
+                </div>
+                <div class="kelas-container20">
+                  <span class="kelas-text08">
+                    Aves, atau lebih dikenal sebagai burung, adalah kelompok
+                    hewan yang memiliki kemampuan terbang. Mereka memiliki
+                    tubuh yang dilengkapi dengan sayap, paruh, bulu, dan telur
+                    yang terbangun dengan cangkang yang kuat. Burung dapat
+                    ditemukan di berbagai habitat di seluruh dunia, mulai dari
+                    darat, air tawar, hingga laut.
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div id="myButton2" class="kelas-pisces">
 
-        <h1>Reptilia</h1>
-        <p>
-            <?php echo $teks; ?>
-        </p>
-    </a>
+            <script>
+                document.getElementById("myButton2").addEventListener("click", function() {
+                  window.location.href = "./list-binatang?kelas=Pisces"
+                });
+              </script>
 
-    <a href="./list-binatang?kelas=Pisces" class="kolom-klik">
-        <?php
-        // Variabel dengan teks yang akan ditampilkan
-        $teks = "Pisces merupakan ikan";
-        ?>
+              <div class="kelas-container21">
+                <div class="kelas-container22"></div>
+              </div>
+              <div class="kelas-container23">
+                <div class="kelas-container24">
+                  <span class="kelas-text09">Pisces</span>
+                </div>
+                <div class="kelas-container25">
+                  <span class="kelas-text10">
+                    Pisces, atau lebih dikenal sebagai ikan, adalah kelompok
+                    hewan yang hidup di dalam air. Mereka memiliki tubuh yang
+                    aerodinamis, bersisik, dan dilengkapi dengan sirip untuk
+                    bergerak di dalam air. Ikan dapat ditemukan di berbagai
+                    habitat air, termasuk sungai, danau, laut, dan terumbu
+                    karang.
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div id="myButton3" class="kelas-reptil">
 
-        <h1>Pisces</h1>
-        <p>
-            <?php echo $teks; ?>
-        </p>
-    </a>
+            <script>
+                document.getElementById("myButton3").addEventListener("click", function() {
+                  window.location.href = "./list-binatang?kelas=Reptil"
+                });
+              </script>
+
+              <div class="kelas-container26">
+                <div class="kelas-container27"></div>
+              </div>
+              <div class="kelas-container28">
+                <div class="kelas-container29">
+                  <span class="kelas-text11">Reptil</span>
+                </div>
+                <div class="kelas-container30">
+                  <span class="kelas-text12">
+                    Reptil adalah kelompok hewan yang termasuk dalam kelas
+                    Reptilia. Mereka memiliki ciri khas seperti tubuh
+                    bersisik, bertelur, dan berdarah dingin. Reptil hidup di
+                    berbagai habitat, mulai dari darat, air, hingga udara.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
